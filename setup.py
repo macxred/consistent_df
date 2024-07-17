99c6878728d0b90adb0d176df29a93ac4b9ab8fd
Alex the wizard <sashok1111122222@gmail.com>
2024-07-17 16:24:37 +0300

    feat: add initial structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="consistent_df",
+    version="v0.0.1",
+    description=("Python package designed to provide shared DataFrame operations"),
+    url='https://github.com/macxred/consistent_df',
+    author="Lukas Elmiger, Oleksandr Stepanenko",
+    python_requires='>3.9',
+    install_requires=['pandas'],
+    packages=find_packages(exclude=('tests', 'examples')),
+    extras_require={
+        "dev": [
+            "flake8",
+            "flake8-import-order",
+            "flake8-docstrings",
+            "flake8-bugbear",
+            "bandit",
+            "safety",
+            "pytest-cov"
+        ]
+    }
+)
